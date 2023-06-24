514df0a249317d3e9701955fa4e6343c697840ec
larsbieber <lbieber1@smail.uni-koeln.de>
2023-06-24 14:46:08 +0200

    Task 2

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -2,10 +2,15 @@ package idh.java;
 
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.StringTokenizer;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.io.FileUtils;
 
 public class Document implements Iterable<String> {
@@ -27,15 +32,67 @@ public class Document implements Iterable<String> {
 		this.documentText = documentText;
 	}
 	
-	public void printStats(File f) {
-		// TODO: Implement
+	public static void printStats(String f, String cat, String res) throws IOException {
+		try (FileWriter wri = new FileWriter(f, true);
+	         CSVPrinter prin = new CSVPrinter(wri,CSVFormat.EXCEL)) {
+				prin.printRecord(cat, res);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
 	}
 	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("src/main/resources/dracula.txt"));
-		d.printStats(new File("target/stats.csv"));
+		for(String token : d) {
+			//System.out.println(token);
+		}
+		numberOfWords(d);
+		numberOfTypes(d);
+		numberOfBelow5(d);
+		numberOfBlood(d);
+		//printStats("src/main/resources/stats.csv", "True", "False");
+	}
+	
+	public static void numberOfWords(Document doc) throws IOException {
+		int i = 0;
+		for(String token : doc) {
+			i++;
+		}
+		printStats("src/main/resources/stats.csv", "Anzahl der Worte", String.valueOf(i));
 	}
 
+	public static void numberOfTypes(Document doc) throws IOException {
+		int tokens = 0;
+		Set<String> types = new HashSet<String>();
+		for (String token : doc) {
+			types.add(token);
+			tokens++;
+		}
+		printStats("src/main/resources/stats.csv", "Anzahl der Types", String.valueOf(types.size()));		
+	}
+	
+	public static void numberOfBelow5(Document doc) throws IOException {
+		int i = 0;
+		for(String token : doc) {
+			if(token.length() < 5) {
+				i++;
+			}
+		}
+		printStats("src/main/resources/stats.csv", "Anzahl der Wörter unter 5 Zeichen", String.valueOf(i));
+	}
+	
+	public static void numberOfBlood(Document doc) throws IOException {
+		int i = 0;
+		for(String token : doc) {
+			if(token.equals("blood") ||  token.equals("Blood")) {
+				i++;
+			}
+		}
+		printStats("src/main/resources/stats.csv", "Anzahl des Wortes 'blood'", String.valueOf(i));
+
+	}
+	
 	public Iterator<String> iterator() {
 		return new Iterator<String>() {
 
