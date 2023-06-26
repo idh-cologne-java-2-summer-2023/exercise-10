adb4c5bc4322bc58b753755af858c7818d83e106
Jago- <Jago-@192.168.2.163>
2023-06-26 20:41:11 +0200

    task 1 done

---
+++ src/main/java/idh/java/Main.java
@@ -0,0 +1,95 @@
+package idh.java;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+
+
+
+public class Main {
+
+    //CSV file location
+    static String csvFileLocation = "src/main/resources/wine.csv";
+
+    //integer marking the number of columns -1 to account for the 0 row
+    static int numberOfRows = 12 - 1;
+
+    public static void main(String[] args) throws IOException {
+        WineCSV program = new WineCSV();
+
+        program.run();
+
+    }
+
+    private static class WineCSV {
+
+        void run() throws IOException {
+
+            parser(0);
+
+        }
+
+        void parser(int row ) throws IOException {
+
+            Reader in = new FileReader(csvFileLocation);
+
+            String consolePrintStream = "";
+
+            double columValue = 0;
+            int columSize = 0;
+
+            try {
+
+                Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(in);
+
+
+                for (CSVRecord record : records) {
+
+                    columValue  += Double.parseDouble(record.get(row));
+
+                    columSize ++;
+
+                    consolePrintStream = "Die Summe der Spalte "
+                            + row
+                            + " beträgt: "
+                            +  columValue /columSize;
+
+                }
+
+                System.out.println("Colum-size: " + columSize + " of colum " + row + ".");
+                System.out.println(consolePrintStream);
+
+                if(row == numberOfRows ){
+
+                    System.out.println(" ");
+                    System.out.println("Process finished with " + row + " checked columns.");
+
+                } else {
+
+                    parser(row +1);
+
+                }
+
+            } catch (FileNotFoundException e) {
+
+                throw new RuntimeException(e);
+
+            } catch (IOException e) {
+
+                throw new RuntimeException(e);
+
+            } finally {
+
+                in.close();
+
+            }
+        }
+    }
+
+
+}
