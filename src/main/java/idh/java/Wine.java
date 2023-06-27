f36dcca2cfc5f379b290d25e3279fa167c09e1a2
javi1337 <jan@pulsarstudio.de>
2023-06-27 20:51:07 +0200

    final commit homework exercise-10

--- src/main/java/idh/java/Wine.java
+++ src/main/java/idh/java/Wine.java
@@ -17,42 +17,41 @@ import org.apache.commons.csv.CSVRecord;
 public class Wine {
 
 	public static void main(String[] args) throws IOException {
-
+		// initialize the variables
 		String path = "src/main/resources/wine.csv";
 		List<Double> values = new ArrayList();
 		int ncol = 0;
 		int nrow = 0;
 
+		// start reader and parser
 		Reader input = new FileReader(path);
 		CSVParser parser = CSVFormat.DEFAULT.parse(input);
 
+		// all values are saved to a list and the number of columns and rows are counted
 		for (CSVRecord item : parser) {
 			ncol = item.size();
 			for (int i = 0; i <= ncol - 1; i++) {
 				values.add(Double.parseDouble(item.get(i)));
 			}
 		}
-
 		nrow = (int) parser.getRecordNumber();
+
+		// loop for the calculation and output of the average of the columns
 		for (int i = 0; i <= ncol - 1; i++) {
-			sum(i, nrow, ncol, values);
+			final DecimalFormat decimal = new DecimalFormat("0.00");
+			double sum = values.get(i);
+			int columnNR = i + 1;
+			double counter = 0;
+			while (counter <= nrow) {
+				sum += values.get(i + ncol);
+				counter++;
+			}
+			System.out.println("Der Durchschnitt von Spalte " + columnNR + " ist: " + decimal.format(sum / nrow));
 		}
 
 		input.close();
 		parser.close();
 
-	}
-
-	public static void sum(int start, int max, int step, List<Double> values) {
-		final DecimalFormat decimal = new DecimalFormat("0.00");
-		double sum = values.get(start);
-		int columnNR = start + 1;
-		double counter = 0;
-		while (counter <= max) {
-			sum += values.get(start + step);
-			counter++;
-		}
-		System.out.println("Der Durchschnitt von Spalte " + columnNR + " ist: " + decimal.format(sum/ max) );
 	}
 
 }
