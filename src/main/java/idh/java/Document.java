b95a6c72d87c3bd117c3567ecc8dadbc9dc1c06d
tamaraneufeld <tamaraneufeld@tamaras-mbp>
2023-06-27 12:48:24 +0200

    exercise10

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -1,57 +1,35 @@
+@@ -0,0 +1,34 @@
 package idh.java;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
-import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.StringTokenizer;
 
-import org.apache.commons.io.FileUtils;
+public class Wein {
 
-public class Document implements Iterable<String> {
-	String documentText;
+    public static void main(String[] args) {
+        String csvFile = "src/main/resources/wine.csv";
+        try {
+            FileReader fileReader = new FileReader(csvFile);
+            CSVParser csvParser = new CSVParser(fileReader, CSVFormat.EXCEL);
 
-	public static Document readFromFile(File f) throws IOException {
-		
-		Document doc = new Document();
-		doc.documentText = FileUtils.readFileToString(f, "UTF-8");
-		
-		return doc;
-	}
-	
-	public String getDocumentText() {
-		return documentText;
-	}
+            for (CSVRecord record : csvParser) {
+                double Summe = 0.0;
+                int Zeile = record.size();
+                for (int i = 0; i < Zeile; i++) {
+                    double Wert = Double.parseDouble(record.get(i));
+                    Summe += Wert;
+                }
+                double Durchschnitt = Summe / Zeile;
+                System.out.println("Durchschnitt Zeile" + record.getRecordNumber() + ": " + Durchschnitt);
+            }
 
-	public void setDocumentText(String documentText) {
-		this.documentText = documentText;
-	}
-	
-	public void printStats(File f) {
-		// TODO: Implement
-	}
-	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("src/main/resources/dracula.txt"));
-		d.printStats(new File("target/stats.csv"));
-	}
-
-	public Iterator<String> iterator() {
-		return new Iterator<String>() {
-
-			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
-			
-			public boolean hasNext() {
-				return tokenizer.hasMoreTokens();
-			}
-
-			public String next() {
-				return tokenizer.nextToken();
-			}
-			
-		};
-	}
-	
-	
+            csvParser.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
