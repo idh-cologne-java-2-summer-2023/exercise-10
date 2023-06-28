e6e429e5a23c279a145ea2b5bd3acb6953ef159e
Balu <120671238+Balu-d@users.noreply.github.com>
2023-06-28 10:10:03 +0200

    redo

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -1,40 +1,57 @@
 package idh.java;
 
+
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
 import org.apache.commons.io.FileUtils;
 
-public class Document {
-    public static void main(String[] args) throws IOException {
-        File csvFile = new File("src/main/resources/wine.csv");
-        List<String> lines = FileUtils.readLines(csvFile, "UTF-8");
+public class Document implements Iterable<String> {
+	String documentText;
 
-        String headerLine = lines.get(0);
-        String[] headers = headerLine.split(",");
-        
-        double[] sumArray = new double[headers.length];
-        Arrays.fill(sumArray, 0.0);
+	public static Document readFromFile(File f) throws IOException {
+		
+		Document doc = new Document();
+		doc.documentText = FileUtils.readFileToString(f, "UTF-8");
+		
+		return doc;
+	}
+	
+	public String getDocumentText() {
+		return documentText;
+	}
 
-        for (int i = 1; i < lines.size(); i++) {
-            String line = lines.get(i);
-            String[] values = line.split(",");
+	public void setDocumentText(String documentText) {
+		this.documentText = documentText;
+	}
+	
+	public void printStats(File f) {
+		// TODO: Implement
+	}
+	
+	public static final void main(String[] args) throws IOException {
+		Document d = Document.readFromFile(new File("src/main/resources/dracula.txt"));
+		d.printStats(new File("target/stats.csv"));
+	}
 
-            for (int j = 0; j < values.length; j++) {
-                double value = Double.parseDouble(values[j]);
-                sumArray[j] += value;
-            }
-        }
+	public Iterator<String> iterator() {
+		return new Iterator<String>() {
 
-        int rowCount = lines.size() - 1;
-        double[] averageArray = new double[sumArray.length];
-        for (int i = 0; i < averageArray.length; i++) {
-            averageArray[i] = sumArray[i] / rowCount;
-        }
+			StringTokenizer tokenizer = new StringTokenizer(documentText);
+			
+			
+			public boolean hasNext() {
+				return tokenizer.hasMoreTokens();
+			}
 
-        for (int i = 0; i < averageArray.length; i++) {
-            System.out.printf("Column %d average: %.2f\n", (i + 1), averageArray[i]);
-        }
-    }
+			public String next() {
+				return tokenizer.nextToken();
+			}
+			
+		};
+	}
+	
+	
 }
