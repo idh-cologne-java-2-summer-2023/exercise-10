868bac12e994b8be2981b99ba275f95a2c718086
Lisa <lisa.krings3@gmail.de>
2023-06-28 11:13:56 +0200

    Homework done

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 import org.apache.commons.io.FileUtils;
 
@@ -27,11 +30,51 @@ public class Document implements Iterable<String> {
 		this.documentText = documentText;
 	}
 	
-	public void printStats(File f) {
-		// TODO: Implement
+	
+	
+	
+	public void printStats(File f) {	 
+	}
+	
+
+	public void printStatsofword(File f, String word) {	 
 	}
 	
 	public static final void main(String[] args) throws IOException {
+		
+		String csvFilePath = "src/main/resources/wine.csv";
+		
+        try (BufferedReader reader = new BufferedReader(new FileReader(csvFilePath))) {
+            String line;
+            int rowCount = 0;
+            double[] columnSums = null;
+
+            while ((line = reader.readLine()) != null) {
+                rowCount++;
+
+                String[] values = line.split(",");
+                if (columnSums == null) {
+                    columnSums = new double[values.length];
+                }
+
+                for (int i = 0; i < values.length; i++) {
+                    double value = Double.parseDouble(values[i]);
+                    columnSums[i] += value;
+                }
+            }
+            
+            System.out.println("Column Averages:");
+
+            for (double sum : columnSums) {
+                double average = sum / rowCount;
+                System.out.println(average);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+   
+        
+		
 		Document d = Document.readFromFile(new File("src/main/resources/dracula.txt"));
 		d.printStats(new File("target/stats.csv"));
 	}
